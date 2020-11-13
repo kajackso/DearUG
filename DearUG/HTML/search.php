31607a3db55364331fa81040bdaa00a8e14fe7fa
siswhitt <55197744+siswhitt@users.noreply.github.com>
2020-11-12 19:36:00 -0500

    bug fixes

--- DearUG/HTML/search.php
+++ DearUG/HTML/search.php
@@ -1,58 +1,60 @@
+<!DOCTYPE html>
 <html>
-   <head>
-   <script type = "text/javascript">
-   function active() {
-           var search_bar = document.getElementById("searchBox");
-           if(search_bar.value == 'Search...') {
-                   search_bar.value = ''
-                   search_bar.placeholder = "Search..."
-           }
-   }
+<link rel="stylesheet" href="format.css">
+  <body>
 
-  function inactive() {
-           var search_bar = document.getElementById("searchBox");
-           if(search_bar.value == 'Search...') {
-                   search_bar.value = ''
-                   search_bar.placeholder = "Search..."
-           }
-   }
-
-   </head>
-   <body>
-   <form action = "" method = "" id = "searchForm"/>
- <?php
+    <h1>Search</h1>
+    <h2>
+      <a href="Main.html">Home</a>
+      <a href="createPost.html">New</a>
+      <a href="Tags.html">Tags</a>
+      <form method="post" action="search.php">
+        <input type="text" id="fname" name="fname" placeholder="Search">
+        <input type="submit" value="Submit">
+      </form>
+      <a href="Login.html">Login</a>
+      <a href="register.php">Register</a>
+    </h2>
+   
+<?php
         $dbhost = 'mysql:host=classdb.it.mtu.edu;port=3307;dbname=fisforsuccess';
         $dbuser = 'fisforsuccess_rw';
         $dbpass = 'success123';
+		
         try {
-                $dbconnect = new PDO($dbhost, $dbuser, $dbpass);
-                echo 'Connected successfully';
-        $query = mysql_query("select t.tagName, p.description, p.name, u.username
-                                                 from tags t inner join post p inner join user u
-                                                 where p.description like '%$in%'
-                                                 or t.tagName like '%$in%'
-                                                 or u.username like '%$in%'");
-
-        $num_rows = mysql_num_rows($query);
-?>
-        <p><strong ><?php echo $num_rows; ?></strong> Results for '<?php echo $in; ?>'</p>
-
-<?php
-        while($row = mysql_fetch_array($query)) {
-                $PostID = $row['PostID'];
-                $TagID = $row['TagID'];
-                $TagName = $row['TagName'];
-                echo $PostID . $TagID . $TagName . '<br/>';
-                }
+			$dbconnect = new PDO($dbhost, $dbuser, $dbpass);
+			$search = $_POST["search"];
+			if(!isset($search)){
+				echo 'fname not given';
+			} else {
+				//Testing, delete later 
+				echo $search;
+				
+				//$statement = $dbconnect -> prepare("CALL search(:search)");
+				//$result = $statement -> execute(array(':search'=> $_POST['search']));
+				
+				$sql = 'CALL search("$search")' ;
+				$result = $dbconnect->query($sql);
+				//$query = mysql_query('CALL search('$_POST["search"]')');
+				//$row = mysql_num_rows($query);
+			
+				//while($result = mysql_fetch_array($statement)){
+				while($row = $result->fetch(PDO::FETCH_ASSOC)){
+					$id = $row["$search"];
+				
+					echo $id;
+				}
+			}
+		}
+			
+		catch (PDOException $error) {
+            die("ERROR: " . $error->getMessage() . "<br/>");
+			
         }
-        catch(PDOException $error){
-                die("ERROR: " . $error . "<br/>");
-        }
 
 
 
 ?>
-
 
 </body>   
 </html>  
