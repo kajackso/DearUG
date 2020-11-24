6cedf9ad1a148f4d2588c6b273686ec9eecedcae
wesley Robert condie <54695431+wrcondie@users.noreply.github.com>
2020-11-24 14:40:45 -0500

    Worked on new post page

--- DearUG/HTML/new.php
+++ DearUG/HTML/new.php
@@ -16,21 +16,21 @@
           $dbconnect = new PDO($dbhost, $dbuser, $dbpass);
 
           //Creating the variables to be used
-          $title = $_POST["title"];
-          $content = $_POST["content"];
+          // $title = $_POST["title"];
+          // $content = $_POST["content"];
           $username = "User"; //Will change to match logged in profile if website is not logged in
 
           //$date = strtotime(time, now);
           //echo "Created date is " . date("Y-m-d h:i:sa", $date);
           //Posting nformation to database using newPost() method
-            // if(isset($_POST["title"])) {
+            if(isset($_POST["title"])) {
                 $statement = $dbconnect -> prepare("CALL newPost(:name, :description, :isEdited, :username, :isArchived)");
                 // $result = $statement -> execute(array(':name'=> $title, ':description'=>$content, ':isEdited' => 0,
                 //   ':username' => $username, ':isArchived' => 0));
                 $result = $statement -> execute(array(':name'=> $_POST["title"], ':description'=> $_POST["content"], ':isEdited' => 0,
                 ':username' => $username, ':isArchived' => 0));
                   header("Location: https://classdb.it.mtu.edu/cs3141/FisForSuccess/action.php");
-            // }
+            }
 
         }
 
@@ -56,7 +56,7 @@
     </h2>
 
     <div class="form">
-      <form action="createPost.html">
+      <form action="new.php">
         <input type="text" id="title" name="title" placeholder="Title"><br/>
         <textarea id="content" name="content" rows="15" cols="75"></textarea><br/>
         <input type="submit" value="Submit">
