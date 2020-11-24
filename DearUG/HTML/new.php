7fd70eb3be721ec8327c8bb28751f6268352bc60
wesley Robert condie <54695431+wrcondie@users.noreply.github.com>
2020-11-24 14:09:04 -0500

    More debuging

--- DearUG/HTML/new.php
+++ DearUG/HTML/new.php
@@ -4,7 +4,41 @@
 <link rel="stylesheet" href="format.css">
 
   <body>
+    <?php
 
+        //Specifies login information for the database
+        $dbhost = 'mysql:host=classdb.it.mtu.edu;port=3307;dbname=fisforsuccess';
+        $dbuser = 'fisforsuccess_rw';
+        $dbpass = 'success123';
+
+        //Attempts to connect to the database
+        try {
+          $dbconnect = new PDO($dbhost, $dbuser, $dbpass);
+
+          //Creating the variables to be used
+          $title = $_POST["title"];
+          $content = $_POST["content"];
+          $username = "User" //Will change to match logged in profile if website is not logged in
+
+          //$date = strtotime(time, now);
+          //echo "Created date is " . date("Y-m-d h:i:sa", $date);
+          //Posting nformation to database using newPost() method
+            if(isset($_POST["title"])) {
+                $statement = $dbconnect -> prepare("CALL newPost(:name, :description, :isEdited, :username, :isArchived)");
+                $result = $statement -> execute(array(':name'=> $title, ':description'=>$content, ':isEdited' => 0,
+                  ':username' => $username, ':isArchived' => 0));
+                  header("Location: https://classdb.it.mtu.edu/cs3141/FisForSuccess/Login.html");
+            }
+
+        }
+
+
+        catch (PDOException $error) {
+                die("ERROR: " . $error->getMessage() . "<br/>");
+
+            }
+
+    ?>
 
     <h1>New Post</h1>
     <!--Banner, links to other pages -->
@@ -27,41 +61,7 @@
       </form>
     </div>
 
-<?php
 
-    //Specifies login information for the database
-    $dbhost = 'mysql:host=classdb.it.mtu.edu;port=3307;dbname=fisforsuccess';
-    $dbuser = 'fisforsuccess_rw';
-    $dbpass = 'success123';
-
-    //Attempts to connect to the database
-    try {
-      $dbconnect = new PDO($dbhost, $dbuser, $dbpass);
-
-      //Creating the variables to be used
-      $title = $_POST["title"];
-      $content = $_POST["content"];
-      $username = "User" //Will change to match logged in profile if website is not logged in
-
-      //$date = strtotime(time, now);
-      //echo "Created date is " . date("Y-m-d h:i:sa", $date);
-      //Posting nformation to database using newPost() method
-        if(isset($_POST["title"])) {
-            $statement = $dbconnect -> prepare("CALL newPost(:name, :description, :isEdited, :username, :isArchived)");
-            $result = $statement -> execute(array(':name'=> $title, ':description'=>$content, ':isEdited' => 0,
-              ':username' => $username, ':isArchived' => 0));
-              header("Location: https://classdb.it.mtu.edu/cs3141/FisForSuccess/Login.html");
-            }
-
-    }
-
-
-    catch (PDOException $error) {
-            die("ERROR: " . $error->getMessage() . "<br/>");
-
-        }
-
-?>
 
 
 
