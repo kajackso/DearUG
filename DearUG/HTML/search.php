344dfb8d0a93125351c350ec29abf95b8f018046
siswhitt <55197744+siswhitt@users.noreply.github.com>
2020-11-30 13:31:01 -0500

    bug fixes

--- DearUG/HTML/search.php
+++ DearUG/HTML/search.php
@@ -17,46 +17,47 @@
     </h2>
 
 <?php
+session_start();
+
         $dbhost = 'mysql:host=classdb.it.mtu.edu;port=3307;dbname=fisforsuccess';
         $dbuser = 'fisforsuccess_rw';
         $dbpass = 'success123';
-
+		
         try {
 			$dbconnect = new PDO($dbhost, $dbuser, $dbpass);
-
+			
 			//Testing, delete later
 			//var_dump($_POST);
-
-			$search = $_POST["search"];
-			if(isset($_POST['search'])){
-
+			
+			$search = $_SESSION['fname'];
+			if(isset($_SESSION['fname'])){
+				
 				//Testing $search = General
 				//echo $search;
-
-				//$statement = $dbconnect -> prepare("CALL search(:search)");
-				//$result = $statement -> execute(array(':search'=> $_POST['search']));
-
-				$sql = ' select* from tags where tagName = "$search"; ';
+				
+				$sql = " select* from tags where tagName =" . $search ;
+				//$result = $dbconnect->query($sql);
+							
 				$result = $dbconnect->query($sql);
-
+				
 				//$query = mysql_query(" CALL search('$search'); ");
 				//$num_rows = mysql_num_rows($query);
+				
 				foreach($result as $row){
-					echo "results";
-					//$id = $row['tag'];
-					//echo $id;
-					echo $row[0];
+					$id = $row[0];
+					$tagId = $row[1];
+					$tagName = $row[2];
+					echo "ID" . $id . "tagID" . $tagId . "tagName" . $tagName;
 				}
-
-
+				
 			} else {
 				echo 'No Results: var not given';
 			}
 		}
-
+			
 		catch (PDOException $error) {
             die("ERROR: " . $error->getMessage() . "<br/>");
-
+			
         }
 
 
