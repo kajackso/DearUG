b9158a7aa0b2bc8b5b5d545a04bb4be2e74f978a
wesley Robert condie <54695431+wrcondie@users.noreply.github.com>
2020-12-01 16:50:40 -0500

    Working on login functionality and debugging

--- DearUG/HTML/action.php
+++ DearUG/HTML/action.php
@@ -1,3 +1,7 @@
+<?php // Initialize the session
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <link rel="stylesheet" href="format.css">
@@ -8,19 +12,28 @@
 
 <body>
 <?php
-// Initialize the session
-session_start();
 
-$dbhost = 'mysql:host=classdb.it.mtu.edu;port=3307;dbname=fisforsuccess';
+
+
+
+
+
+//$dbhost = 'mysql:host=classdb.it.mtu.edu;port=3307;dbname=fisforsuccess';
+$dbhost = 'classdb.it.mtu.edu';
+$dbport = '3307';
+$dbname = 'fisforsuccess';
 $dbuser = 'fisforsuccess_rw';
 $dbpass = 'success123';
 
-$link = new PDO($dbhost, $dbuser, $dbpass);
+$link = new mysqli($dbhost, $dbuser, $dbpass,$dbname,$dbport);
+
+
+//$_SESSION["username"] = "defualt";
 
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: Main.html");
-    exit;
+    //header("location: Main.html");
+    //exit;
 }
 
 
@@ -31,17 +44,25 @@ $username_err = $password_err = "";
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
+    $password = $_POST["password"];
+    $username = $_POST["username"];
+
+
     // Validate credentials
     if(empty($username_err) && empty($password_err)){
         // Prepare a select statement
-        $sql = "SELECT username, password FROM users WHERE username = ?";
+        $sql = "SELECT username, password FROM user WHERE username = ?";
+        //"select username from user where encrypt(?) = user.password andwhere $username = user.username";
+        //if sql != null;
+
 
         if($stmt = mysqli_prepare($link, $sql)){
+        //if($stmt = $link -> prepare("SELECT username, password FROM users WHERE username = ?")){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
-
+            mysqli_stmt_bind_param($stmt, 's', $param_username);
             // Set parameters
             $param_username = $username;
+            //$_SESSION["username"] = $username;
 
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
@@ -51,19 +72,23 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 // Check if username exists, if yes then verify password
                 if(mysqli_stmt_num_rows($stmt) == 1){
                     // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
-                    if(mysqli_stmt_fetch($stmt)){
-                        if(password_verify($password, $hashed_password)){
-                            // Password is correct, so start a new session
-                            session_start();
+                    mysqli_stmt_bind_result($stmt, $username, $hashed_password); //$id,
 
+
+                    if(mysqli_stmt_fetch($stmt)){
+                        //if(password_verify($password, $hashed_password)){
+                        if($password == $hashed_password) {
+                            // Password is correct, so start a new session
+                            //session_start();
+
+                            //$_SESSION["id"] = $id;
+                            $_SESSION["username"] = $_POST["username"];
                             // Store data in session variables
                             $_SESSION["loggedin"] = true;
-                            //$_SESSION["id"] = $id;
-                            $_SESSION["username"] = $username;
+
 
                             // Redirect user to main page
-                            header("location: Main.html");
+                            //header("location: Main.html");
                         } else{
                             // Display an error message if password is not valid
                             $password_err = "The password you entered was not valid.";
@@ -107,14 +132,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <form method="post" action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>
             <div class="form-group" action=<?php echo (!empty($username_err)) ? 'has-error' : ''; ?>
                 <label>Username</label>
-                <input type="text" name="username" class="form-control" placeholder="Username">
+                <input type="text" id="username" name="username" class="form-control" placeholder="Username">
                 <span class="help-block"><?php echo $username_err; ?></span>
             </div>
 
 
             <div class=form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>
                 <label>Password</label>
-                <input type="password" name="password" class="form-control" placeholder="Password">
+                <input type="password" id="password" name="password" class="form-control" placeholder="Password">
                 <span class="help-block"><?php echo $password_err; ?></span>
             </div>
 
--- DearUG/HTML/new.php
+++ DearUG/HTML/new.php
@@ -1,3 +1,7 @@
+<?php // Initialize the session
+  session_start();
+?>
+
 <!DOCTYPE html>
 
 <html lang="en" dir="ltr">
@@ -5,6 +9,8 @@
 
   <body>
     <?php
+    echo "wow";
+        //session_start();
 
         //Specifies login information for the database
         $dbhost = 'mysql:host=classdb.it.mtu.edu;port=3307;dbname=fisforsuccess';
@@ -13,32 +19,42 @@
 
         //Attempts to connect to the database
         try {
+          echo "cool";
           $dbconnect = new PDO($dbhost, $dbuser, $dbpass);
-
+          if ($dbconnect) {
+            echo "help";
+          }
           //Creating the variables to be used
           // $title = $_POST["title"];
           // $content = $_POST["content"];
-          $username = "User"; //Will change to match logged in profile if website is not logged in
+          //$username = $_SESSION["username"]; //Will change to match logged in profile if website is not logged in
+          //if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+              //echo "Favorite animal is " . $_SESSION["username"] . ".";
+
 
           //$date = strtotime(time, now);
           //echo "Created date is " . date("Y-m-d h:i:sa", $date);
           //Posting nformation to database using newPost() method
-            if(isset($_POST["title"])) {
+          
+            if(isset($_POST["title"]) || !empty($_POST["title"])) {
+              echo " wesley ";
                 $statement = $dbconnect -> prepare("CALL newPost(:name, :description, :isEdited, :username, :isArchived)");
                 // $result = $statement -> execute(array(':name'=> $title, ':description'=>$content, ':isEdited' => 0,
                 //   ':username' => $username, ':isArchived' => 0));
                 $result = $statement -> execute(array(':name'=> $_POST["title"], ':description'=> $_POST["content"], ':isEdited' => 0,
-                ':username' => $username, ':isArchived' => 0));
-                  header("Location: https://classdb.it.mtu.edu/cs3141/FisForSuccess/action.php");
+                ':username' => $_SESSION["username"], ':isArchived' => 0));
+                  header("Location: https://classdb.it.mtu.edu/cs3141/FisForSuccess/Main.html");
             }
 
-        }
+        //}
+      }
 
 
         catch (PDOException $error) {
                 die("ERROR: " . $error->getMessage() . "<br/>");
 
             }
+
 
     ?>
 
@@ -56,7 +72,7 @@
     </h2>
 
     <div class="form">
-      <form action="new.php">
+      <form  method=post action="new.php">
         <input type="text" id="title" name="title" placeholder="Title"><br/>
         <textarea id="content" name="content" rows="15" cols="75"></textarea><br/>
         <input type="submit" value="Submit">
