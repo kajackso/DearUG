a69a98ad1e23ba5feb475927feb84d7e370551cc
zjnoel <zjnoel@mtu.edu>
2020-12-04 18:39:55 -0500

    Added checks to make sure user is logged in before creating a post

--- DearUG/HTML/new.php
+++ DearUG/HTML/new.php
@@ -9,7 +9,6 @@
 
   <body>
     <?php
-
         //Specifies login information for the database
         $dbhost = 'mysql:host=classdb.it.mtu.edu;port=3307;dbname=fisforsuccess';
         $dbuser = 'fisforsuccess_rw';
@@ -17,28 +16,25 @@
 
         //Attempts to connect to the database
         try {
-
           $dbconnect = new PDO($dbhost, $dbuser, $dbpass);
 
           //If the form is filled in when the page loads, this pushes the content to the database
-            if(isset($_POST["title"]) || !empty($_POST["title"])) {
-              echo " wesley ";
+            if(isset($_POST["title"]) && !empty($_POST["title"]) && isset($_POST["content"]) && !empty($_POST["content"])) {
+              if(isset($_SESSION['username']) && !empty($_SESSION['username'])){
                 $statement = $dbconnect -> prepare("CALL newPost(:name, :description, :isEdited, :username, :isArchived)");
                 $result = $statement -> execute(array(':name'=> $_POST["title"], ':description'=> $_POST["content"], ':isEdited' => 0,
                 ':username' => $_SESSION["username"], ':isArchived' => 0));
-                  header("Location: https://classdb.it.mtu.edu/cs3141/FisForSuccess/Main.html");
+                header("Location: https://classdb.it.mtu.edu/cs3141/FisForSuccess/Main.html");
+              } else {
+                header("refresh:0;url=https://classdb.it.mtu.edu/cs3141/FisForSuccess/action.php");
+                echo "<script>alert(\"Please login before creating a post. Sending to the login page...\")</script>";
+              }
             }
-
-        //}
-      }
-
-
+        }
         catch (PDOException $error) {
                 die("ERROR: " . $error->getMessage() . "<br/>");
 
             }
-
-
     ?>
 
     <h1>New Post</h1>
